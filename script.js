8b5dc7c7b5d99ab1bc9c4e7962bf0de677d98cbc
SachinK27 <sachinkumar1673@gmail.com>
2024-01-12 11:39:27 +0530

    added slider indicator

--- script.js
+++ script.js
@@ -60,3 +60,24 @@ $(".slider").slick({
     	$('.single-item').slick('slickGoTo', goToThisIndex, false);
     	startProgressbar();
     });
+
+    $(document).ready(function () {
+    	jonu(10);
+    });
+
+    function jonu(x){
+        document.getElementById('hel').setAttribute("r", x);
+          if(x==0||x<40){
+          x=x+1;
+          var a=setTimeout('jonu('+x+')',15) ;   
+          }
+        
+        }
+        function jon(l){
+         document.getElementById('hel').setAttribute("r", l);
+           if(l<40 && l>10){
+              l=l-1;
+              var a=setTimeout('jon('+l+')',15) ; 
+           }
+           
+        }
