143d3c775a2232c6a882dd788a1c56c8d727376f
SachinK27 <sachinkumar1673@gmail.com>
2024-01-12 13:12:52 +0530

    bold heading

--- script.js
+++ script.js
@@ -61,23 +61,13 @@ $(".slider").slick({
     	startProgressbar();
     });
 
-    $(document).ready(function () {
-    	jonu(10);
-    });
+    
+const speed = 10;
 
-    function jonu(x){
-        document.getElementById('hel').setAttribute("r", x);
-          if(x==0||x<40){
-          x=x+1;
-          var a=setTimeout('jonu('+x+')',15) ;   
-          }
-        
-        }
-        function jon(l){
-         document.getElementById('hel').setAttribute("r", l);
-           if(l<40 && l>10){
-              l=l-1;
-              var a=setTimeout('jon('+l+')',15) ; 
-           }
-           
-        }
+const moveBackground = () => {
+    let mouseXPos = (event.x / window.innerWidth)*100;
+    let mouseYPos = (event.y / window.innerHeight)*100;
+    document.getElementById('anim').style.backgroundPosition = `${mouseXPos / speed}% ${mouseYPos / speed}%`;
+}
+
+document.getElementById('textanim').addEventListener('mousemove', moveBackground);
